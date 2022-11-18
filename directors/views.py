8e7c0c411304f7a5ea3fc68aacc4bd2b573d3a96
Gonzalo Gonzalez <107937050+gg161087@users.noreply.github.com>
2022-11-17 22:19:34 -0300

    Final Commit

--- directors/views.py
+++ directors/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def index(request):
+    num_films = Film.objects.all().count()
+    num_instances = FilmInstance.objects.all().count()
+    num_authors = Author.objects.all().count()
+
+    available = FilmInstance.objects.filter(status__exact='a').count()
+    
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_films': num_films,
+            'num_instances': num_instances,
+            'num_authors': num_authors,
+            'available': available
+        }
+    )
